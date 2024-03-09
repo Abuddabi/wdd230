0daa888323edb80d95027782b74868eef4eec549
Abuddabi <web.abudabi@gmail.com>
2024-03-09 13:24:34 -0600

    chamber. last modified fex

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -4,3 +4,6 @@ const nav = mobMenuBtn.parentNode;
 mobMenuBtn.addEventListener("click", function () {
   nav.classList.toggle("js-mobile-open");
 });
+
+const lastModifiedEl = document.querySelector("#js-last-mod");
+lastModifiedEl.textContent = document.lastModified;
